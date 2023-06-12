7f168f391a5cd6c019dbd918325bfd0d072fcfa2
이여진 <jinspace3710@gmail.com>
2023-06-13 01:13:33 +0900

    feat: 영화 데이터 추가

---
+++ src/data/movieData.js
@@ -0,0 +1,32 @@
+export const MOVIE_DATA = [
+  {
+    id: 1,
+    name: '라일라일크로커다일',
+    review: '노래하는 악어 짱귀여워요!',
+    img: './images/movie1.jpg',
+  },
+  {
+    id: 2,
+    name: '아바타: 물의 길',
+    review: '물속에서 영화보는 기분!',
+    img: './images/movie2.jpg',
+  },
+  {
+    id: 3,
+    name: '앤트맨과 와스프: 퀀텀매니아',
+    review: '굉장히 복잡해지는 마블 세계관..',
+    img: './images/movie3.jpg',
+  },
+  {
+    id: 4,
+    name: '던전 앤 드래곤: 도적들의 명예',
+    review: '시원시원한 영화! 속편 나왔으면 좋겠어요!',
+    img: './images/movie4.jpg',
+  },
+  {
+    id: 5,
+    name: '파벨만스',
+    review: '엔딩 장면이 기억에 남아요!',
+    img: './images/movie5.jpg',
+  },
+];
